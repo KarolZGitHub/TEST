6295e9861b2fa696a5ebdb771b37ef03654d5f0c
Karol <karol.zimolag@wp.pl>
2024-02-18 13:34:59 +0100

    Changed import.sql, and time views.

--- src/main/java/com/employee/employeeandworkordermanagement/controller/WorkingDurationController.java
+++ src/main/java/com/employee/employeeandworkordermanagement/controller/WorkingDurationController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Controller
 @RequiredArgsConstructor
